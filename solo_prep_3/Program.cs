0a11d60e95fd1dd3230c08189a74ec7eaac4a464
Scott Burton <burtons@byui.edu>
2021-09-02 16:27:33 -0600

    Finished solo prep 2

---
+++ solo_prep_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace solo_prep_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
