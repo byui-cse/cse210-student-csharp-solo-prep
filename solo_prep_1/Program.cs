e16f7e83e93f652370442e34039fb5b73c5a5f7b
Scott Burton <burtons@byui.edu>
2021-09-03 12:37:58 -0600

    Updated to match output formatting

--- solo_prep_1/Program.cs
+++ solo_prep_1/Program.cs
@@ -6,10 +6,10 @@ namespace solo_prep_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your first name? ");
+            Console.Write("What is your first name? ");
             string first = Console.ReadLine();
 
-            Console.WriteLine("What is your last name? ");
+            Console.Write("What is your last name? ");
             string last = Console.ReadLine();
 
             Console.WriteLine($"Your name is {last}, {first} {last}");
